e68f8c9a15c3a3f76c34b12c99ade2d7bac2605c
matdieuhau999 <42971823+matdieuhau999@users.noreply.github.com>
2019-12-09 16:30:31 +0700

    Update clear.sh

--- 5/1/clear.sh
+++ 5/1/clear.sh
@@ -1,15 +1,9 @@
 docker system prune
-
 docker system prune --volumes
-
 docker image prune
-
 docker container prune
-
 docker container stop $(docker container ls -aq)
-
 docker container rm $(docker container ls -aq)
-
 docker rmi $(docker images -q)
 
 
